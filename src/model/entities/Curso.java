5b29f7090b727bda37c3083f5acd732503767789
Yasmin Castro <yasminsdcastro@gmail.com>
2022-05-11 19:57:32 -0300

    Add files via upload

---
+++ src/model/entities/Curso.java
@@ -0,0 +1,45 @@
+package model.entities;
+
+public class Curso {
+	private Integer idcurso;
+	private String nomeCurso;
+	
+	public Curso(Integer idcurso, String nomeCurso) {
+		
+		this.idcurso = idcurso;
+		this.nomeCurso = nomeCurso;
+	}
+
+	public Curso() {
+		
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Curso [idcurso=" + idcurso + ", nomeCurso=" + nomeCurso + "]";
+	}
+	
+
+	public Integer getIdcurso() {
+		return idcurso;
+	}
+
+	public void setIdcurso(Integer idcurso) {
+		this.idcurso = idcurso;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	
+	
+	
+	
+}
